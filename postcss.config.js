67212578160aed0066ec3c86107694d49b950419
jbm <josue@rigorous-digital.co.uk>
2021-06-18 16:09:49 -0500

    feat: tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 module.exports = {
     plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
         require('autoprefixer'),
     ],
 };
