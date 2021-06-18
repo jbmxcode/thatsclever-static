f4fe16525148b23641aa8b17cd7757f8b0e143f2
jbm <josue@rigorous-digital.co.uk>
2021-06-18 15:43:06 -0500

    refactor: webpack 5

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -1,22 +1,22 @@
 module.exports = {
     ci: {
-      collect: {
-        startServerCommand: 'npm run start',
-        numberOfRuns: 3,
-        url: [
-          'http://localhost:8080',
-        ],
-      },
-      assert: {
-        assertions: {
-          'categories:performance': ['error', {minScore: .7}],
-          'categories:accessibility': ['error', {minScore: .7}],
-          'categories:best-practices': ['error', {minScore: .7}],
-          'categories:seo': ['error', {minScore: .7}],
-        }
-      },
-      upload: {
-        target: "temporary-public-storage",
-      },
+        collect: {
+            startServerCommand: 'npm run test',
+            numberOfRuns: 3,
+            url: [
+                'http://localhost:8080',
+            ],
+        },
+        assert: {
+            assertions: {
+                'categories:performance': ['error', { minScore: 0.8 }],
+                'categories:accessibility': ['error', { minScore: 0.8 }],
+                'categories:best-practices': ['error', { minScore: 0.8 }],
+                'categories:seo': ['error', { minScore: 0.8 }],
+            },
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
     },
-  };
+};
--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,14 @@
 module.exports = {
     ci: {
         collect: {
-            startServerCommand: 'npm run start',
+            startServerCommand: 'npm run test',
             numberOfRuns: 1,
             url: [
                 'http://localhost:8080',
             ],
         },
         upload: {
-            target: "temporary-public-storage",
+            target: 'temporary-public-storage',
         },
     },
 };
